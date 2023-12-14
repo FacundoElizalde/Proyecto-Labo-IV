ac445838341b5e4fc61130633e1defab4c288620
Gonzalo <113151385+GonzaloStickar@users.noreply.github.com>
2023-12-13 21:14:13 -0300

    peliculas inicio listview
    Empezando a poner la lista de películas a mostrar (10).

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_repo_guide/widgets/drawer_menu.dart';
 
+class Movie {
+  final String title;
+  final String director;
+  final int year;
+
+  Movie({
+    required this.title,
+    required this.director,
+    required this.year,
+  });
+}
+
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
+
+  final List<Movie> movies = [
+    Movie(
+  title: 'Inception',
+  director: 'Christopher Nolan',
+  year: 2010,
+  releaseDate: 'July 16, 2010',
+  synopsis: 'A thief who enters the dreams of others to steal their secrets.',
+  imageUrl: 'https://mario.nintendo.com/static/f350c31adcd6378b913f7660db299714/7e15c/mario.png', // Asegúrate de que esta URL sea válida
+),
+
+    // ... Agrega más películas según sea necesario
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -12,17 +37,56 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
       ),
       drawer: DrawerMenu(),
-      body: const Center(child: Text('Widget')),
+      body: ListView.builder(
+        itemCount: movies.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(movies[index].title),
+            subtitle: Text('${movies[index].director} (${movies[index].year})'),
+            onTap: () {
+              // Implementa la lógica cuando se selecciona una película
+              // Puedes navegar a una pantalla de detalles, por ejemplo
+              _showMovieDetails(context, movies[index]);
+            },
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print('hice click en el boton');
+          print('Hiciste clic en el botón');
         },
-        child: const Icon(
-          Icons.add,
-          size: 30,
-        ),
+        child: const Icon(Icons.add, size: 30),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
+  }
+
+  void _showMovieDetails(BuildContext context, Movie movie) {
+    // Implementa la lógica para mostrar los detalles de la película
+    // Puedes abrir una nueva pantalla o mostrar un cuadro de diálogo, por ejemplo
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(movie.title),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Director: ${movie.director}'),
+              Text('Year: ${movie.year}'),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
