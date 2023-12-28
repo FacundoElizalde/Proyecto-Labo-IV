31e8205534f6c00df82f632c02b41a21d660842a
Facundo Elizalde <faculobitoelizalde@gmail.com>
2023-12-28 18:02:30 -0300

    Eliminacion de archivos innecesarios (Gonzalo y Facundo)

--- lib/screens/form_page.dart
+++ lib/screens/form_page.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_repo_guide/themes/default_theme.dart';
-
 
 import '../widgets/inputs_form.dart';
 
 class FormScreen extends StatefulWidget {
-   
   const FormScreen({super.key});
 
   @override
@@ -13,25 +10,23 @@ class FormScreen extends StatefulWidget {
 }
 
 class _FormScreenState extends State<FormScreen> {
-
   final Map<String, dynamic> valuesForm = {
-      'firstname'    : '',
-      'lastname'     : '',
-      'email'        : '',
-      'dni'          : '',
-      'password'     : '',
-    };
+    'firstname': '',
+    'lastname': '',
+    'email': '',
+    'dni': '',
+    'password': '',
+  };
 
   final GlobalKey<FormState> keyForm = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar:AppBar(         
-         centerTitle:true,
-         title: const Text('Registrarse'),
-         elevation:10,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Registrarse'),
+        elevation: 10,
       ),
       body: Padding(
         padding: const EdgeInsets.all(25.0),
@@ -40,37 +35,70 @@ class _FormScreenState extends State<FormScreen> {
             key: keyForm,
             child: Column(
               children: [
-                InputsForm(labelText: 'Nombre', inputName: 'firstname',inputValues: valuesForm,),
-                const SizedBox(height: 15,),
-                InputsForm(labelText: 'Apellido',inputName: 'lastname',inputValues: valuesForm,),
-                const SizedBox(height: 15,),
-                InputsForm(labelText:'DNI', maxLength: 8, helperText: 'EJ.22123456',keyboardType: TextInputType.number, inputName: 'dni',inputValues: valuesForm,),
-                const SizedBox(height: 15,),
-                InputsForm(helperText: 'Ej: name@dominio.com',keyboardType: TextInputType.emailAddress,suffixIcon: Icons.email, inputName: 'email',inputValues: valuesForm,),
-                const SizedBox(height: 15,),
-                InputsForm(helperText: 'Password',keyboardType: TextInputType.visiblePassword, suffixIcon: Icons.password, inputName: 'password',inputValues: valuesForm,),
-                const SizedBox(height: 30,),
-
+                InputsForm(
+                  labelText: 'Nombre',
+                  inputName: 'firstname',
+                  inputValues: valuesForm,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                InputsForm(
+                  labelText: 'Apellido',
+                  inputName: 'lastname',
+                  inputValues: valuesForm,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                InputsForm(
+                  labelText: 'DNI',
+                  maxLength: 8,
+                  helperText: 'EJ.22123456',
+                  keyboardType: TextInputType.number,
+                  inputName: 'dni',
+                  inputValues: valuesForm,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                InputsForm(
+                  helperText: 'Ej: name@dominio.com',
+                  keyboardType: TextInputType.emailAddress,
+                  suffixIcon: Icons.email,
+                  inputName: 'email',
+                  inputValues: valuesForm,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                InputsForm(
+                  helperText: 'Password',
+                  keyboardType: TextInputType.visiblePassword,
+                  suffixIcon: Icons.password,
+                  inputName: 'password',
+                  inputValues: valuesForm,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
                 ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red
-                      ),
-                     child: const SizedBox(
-                       width: double.infinity,
-                       height: 50,
-                       child: Center(child: const Text('Guardar'))
-                      ),
-                     onPressed: () {
-                        print(valuesForm);
-                        FocusManager.instance.primaryFocus?.unfocus();
-        
-                        if ( !keyForm.currentState!.validate() ) {
-                          print('Formulario no válido');
-                          return;
-                        }                      
-                        print(valuesForm);
-                     }, 
-                    )
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                  child: const SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: Center(child: const Text('Guardar'))),
+                  onPressed: () {
+                    print(valuesForm);
+                    FocusManager.instance.primaryFocus?.unfocus();
+
+                    if (!keyForm.currentState!.validate()) {
+                      print('Formulario no válido');
+                      return;
+                    }
+                    print(valuesForm);
+                  },
+                )
               ],
             ),
           ),
@@ -79,4 +107,3 @@ class _FormScreenState extends State<FormScreen> {
     );
   }
 }
-
